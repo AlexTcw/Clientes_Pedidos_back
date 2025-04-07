6bfce70ebd04ee1f0ccdbad194b6e9c84bf21092
Edgar Alexis Tavera Carbajal <105955484+AlexTcw@users.noreply.github.com>
2025-04-06 19:31:04 -0600

    first commit

---
+++ src/main/java/com/cpb/ClientesPedidosBackApplication.java
@@ -0,0 +1,13 @@
+package com.cpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesPedidosBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesPedidosBackApplication.class, args);
+    }
+
+}
